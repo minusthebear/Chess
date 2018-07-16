4d082c7181f6029925d37efff6cf1e894fe9500b
matthewhamann <matt.hamann1982@gmail.com>
2018-07-15 22:13:19 -0700

    Edited King.js - straight and diagonal moves

--- King.js
+++ King.js
@@ -3,20 +3,19 @@
 var Piece = require('./Piece'),
     Grid = require('./Grid');
 
-var Queen = function(x, y, type, color) {
+var King = function(x, y, type, color) {
     Piece.call(this, x, y, type, color);
 };
 
-Queen.prototype = Object.create(Piece.prototype);
+King.prototype = Object.create(Piece.prototype);
 
-Queen.prototype.moveStraight = function(x, y, grid) {
+King.prototype.moveStraight = function(x, y, grid) {
 
     if (!grid.boundaryCheck(x, y)) {
         return false;
     }
 
-    var yToCheck,
-        oldX = this.position.x,
+    var oldX = this.position.x,
         oldY = this.position.y,
         board = grid.grid;
 
@@ -24,96 +23,37 @@ Queen.prototype.moveStraight = function(x, y, grid) {
         return false;
     }
 
-    if (oldX === x && oldY > y) {
-        for (var i = oldY - 1; i >= y; i--) {
-            if (board[x][i]) {
+    if (oldX !== x && Math.abs(oldX - x) !== 1) {
+        return false;
+    }
 
-                var oldObj;
+    if (oldY !== y && Math.abs(oldY - y) !== 1) {
+        return false;
+    }
 
-                if (this.checkIfOppositeColor(board, x, i)) {
-                    oldObj = board[x][i];
-                    this.setGridStraight(grid, x, oldX, i, oldY);
-                    this.unTouched = false;
-                    grid.splicePiece(oldObj);
-                    return true;
-                } else {
-                    return false;
-                }
+    try {
+        if (board[x][y]) {
+
+            var oldObj;
+
+            if (this.checkIfOppositeColor(board, x, y)) {
+                oldObj = board[x][y];
+                grid.splicePiece(oldObj);
+            } else {
+                return false;
             }
         }
         this.setGridStraight(grid, x, oldX, y, oldY);
         this.unTouched = false;
-        return true;
+
+    } catch (err) {
+        return false;
     }
 
-    if (oldX === x && oldY < y) {
-        for (var i = oldY + 1; i <= y; i++) {
-            if (board[x][i]) {
-
-                var oldObj;
-
-                if (this.checkIfOppositeColor(board, x, i)) {
-                    var oldObj = board[x][i];
-                    this.setGridStraight(grid, x, oldX, i, oldY);
-                    this.unTouched = false;
-                    grid.splicePiece(oldObj);
-                    return true;
-                } else {
-                    return false;
-                }
-            }
-        }
-        this.setGridStraight(grid, x, oldX, y, oldY);
-        this.unTouched = false;
-        return true;
-    }
-
-    if (oldY === y && oldX > x) {
-        for (var i = oldX - 1; i >= x; i--) {
-            if (board[i][y]) {
-
-                var oldObj;
-
-                if (this.checkIfOppositeColor(board, i, y)) {
-                    var oldObj = board[i][y];
-                    this.setGridStraight(grid, i, oldX, y, oldY);
-                    this.unTouched = false;
-                    grid.splicePiece(oldObj);
-                    return true;
-                } else {
-                    return false;
-                }
-            }
-        }
-        this.setGridStraight(grid, x, oldX, y, oldY);
-        this.unTouched = false;
-        return true;
-    }
-
-    if (oldY === y && oldX < x) {
-        for (var i = oldX + 1; i <= x; i++) {
-            if (board[i][y]) {
-
-                var oldObj;
-
-                if (this.checkIfOppositeColor(board, i, y)) {
-                    var oldObj = board[i][y];
-                    this.setGridStraight(grid, i, oldX, y, oldY);
-                    grid.splicePiece(oldObj);
-                    this.unTouched = false;
-                    return true;
-                } else {
-                    return false;
-                }
-            }
-        }
-        this.setGridStraight(grid, i, oldX, y, oldY);
-        this.unTouched = false;
-        return true;
-    }
+    return true;
 };
 
-Queen.prototype.moveDiagonal = function(x, y, grid) {
+King.prototype.moveDiagonal = function(x, y, grid) {
 
     if (!grid.boundaryCheck(x, y)) {
         return false;
@@ -122,75 +62,23 @@ Queen.prototype.moveDiagonal = function(x, y, grid) {
     var yToCheck,
         oldX = this.position.x,
         oldY = this.position.y,
-        color = this.color;
+        xToCheck = Math.abs(oldX - x),
+        yToCheck = Math.abs(oldY - y);
 
     if (oldY === y || oldX === x) {
         return false;
     }
 
-    if (oldX > x && oldY > y) {
-        var xToCheck = oldX - x,
-            yToCheck = oldY - y;
-
-        if (xToCheck !== yToCheck) {
-            return false;
-        }
-
-        for (var i = 1; i <= xToCheck; i++) {
-            if (grid.grid[oldX - i][oldY - i]) {
-                return this.checkPiece.apply(this, [grid, oldX - 1, oldX, oldY - 1, oldY]);
-            }
-        }
+    if (xToCheck !== yToCheck || xToCheck !== 1) {
+        return false;
     }
 
-    if (oldX > x && oldY < y) {
-        var xToCheck = oldX - x,
-            yToCheck = y - oldY;
-
-        if (xToCheck !== yToCheck) {
-            return false;
-        }
-
-        for (var i = 1; i <= xToCheck; i++) {
-            if (grid.grid[oldX - i][oldY + i]) {
-                return this.checkPiece.apply(this, [grid, oldX - 1, oldX, oldY + i, oldY]);
-            }
-        }
-    }
-
-    if (oldX < x && oldY > y) {
-        var xToCheck = x - oldX,
-            yToCheck = oldY - y;
-
-        if (xToCheck !== yToCheck) {
-            return false;
-        }
-
-        for (var i = 1; i <= xToCheck; i++) {
-            if (grid.grid[oldX + i][oldY - i]) {
-                return this.checkPiece.apply(this, [grid, oldX + 1, oldX, oldY + i, oldY]);
-            }
-        }
-    }
-
-    if (oldX < x && oldY < y) {
-        var xToCheck = x - oldX,
-            yToCheck = y - oldY;
-
-        if (xToCheck !== yToCheck) {
-            return false;
-        }
-
-
-        for (var i = 1; i <= xToCheck; i++) {
-            if (grid.grid[oldX - i][oldY - i]) {
-                return this.checkPiece.apply(this, [grid, oldX + 1, oldX, oldY + i, oldY]);
-            }
-        }
+    if (grid.grid[x][y]) {
+        return this.checkPiece.apply(this, [grid, oldX - 1, oldX, oldY - 1, oldY]);
     }
 };
 
-Queen.prototype.setGridStraight = function(grid, x, oldX, y, oldY) {
+King.prototype.setGridStraight = function(grid, x, oldX, y, oldY) {
 
     if (!x && !y && ((!oldX && !oldY) || (oldY && oldX))) {
         return false;
@@ -200,7 +88,7 @@ Queen.prototype.setGridStraight = function(grid, x, oldX, y, oldY) {
     grid.setPiece(x, y, oldX, oldY, this);
 };
 
-Queen.prototype.setGridDiagonal = function(grid, x, oldX, y, oldY) {
+King.prototype.setGridDiagonal = function(grid, x, oldX, y, oldY) {
 
     if (!x && !y && !oldX && !oldY) {
         return false;
@@ -212,20 +100,11 @@ Queen.prototype.setGridDiagonal = function(grid, x, oldX, y, oldY) {
     }
 };
 
-Queen.prototype.checkPiece = function(grid, numX, oldNumX, numY, oldNumY) {
+King.prototype.checkPiece = function(grid, numX, oldNumX, numY, oldNumY) {
     var oldObj;
 
     if (this.checkIfOppositeColor.apply(this, [grid.grid, numX, numY])) {
         oldObj = grid.grid[numX][numY];
-        this.setGridDiagonal.apply(this, [grid, numX, oldNumX, numY, oldNumY]);
-        grid.splicePiece(oldObj);
-        return true;
-    } else {
-        return false;
-    }
-};
-
-module.exports = Queen;rid[numX][numY];
         this.setGridDiagonal.apply(this, [grid, numX, oldNumX, numY, oldNumY]);
         grid.splicePiece(oldObj);
         return true;
